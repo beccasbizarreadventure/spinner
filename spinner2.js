5149c8f4ef8a37216e0ae526d3e91c3b4e66fe65
Rebecca <rebecca90469@gmail.com>
2024-02-11 11:09:37 -0500

    Refactored spinner animation

---
+++ spinner2.js
@@ -0,0 +1,6 @@
+const timerCharacter = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   '];
+for (let i = 0; i < timerCharacter.length; i++) {
+  setTimeout(() => {
+    process.stdout.write(timerCharacter[i]);
+  }, i * 200);
+};
